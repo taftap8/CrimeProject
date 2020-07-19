497651efde177f9c4535f042d166a4edcb285c85
Alexandra Taft <taft.alexandra@gmail.com>
2020-07-19 16:00:37 -0400

    PowerPoint

---
+++ api_keys.py
@@ -0,0 +1,12 @@
+
+api_key_s = "297x5dnc2mruzg3sxzt43cfrlsr92d6aoqf5u7xocsyirksbyd"
+	
+
+api_key= "4uvk5nnqk6h636ncjdu3vdszb"
+	
+
+AppToken = "NUpzmd6G0IIlf58725Bh4nJo6"
+	
+
+# Google API Key
+g_key = "AIzaSyCi670bGpAfP3Y7o2bZM1TKeYq0Ew_0lMY"
